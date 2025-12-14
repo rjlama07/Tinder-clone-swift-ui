e7a553aebe7900bd17a427d319f94fdae2c7b710
Rj lama <riteshlama5@gmail.com>
2025-12-14 20:10:14 +1100

    Initial Commit

---
+++ Tinder Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tinder Clone
+//
+//  Created by Ritesh jung lama on 14/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tinder Clone/Tinder_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinder_CloneApp.swift
+//  Tinder Clone
+//
+//  Created by Ritesh jung lama on 14/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Tinder_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
