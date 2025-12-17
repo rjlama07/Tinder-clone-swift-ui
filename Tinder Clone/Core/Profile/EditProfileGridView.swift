8eb4391e4702e54dd25a081aead1af384efab872
Rj lama <riteshlama5@gmail.com>
2025-12-17 14:19:18 +1100

    Edit proile

---
+++ Tinder Clone/Core/Profile/EditProfileGridView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileGridView.swift
+//  Tinder Clone
+//
+//  Created by Ritesh jung lama on 17/12/2025.
+//
+
+import SwiftUI
+
+struct EditProfileGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileGridView()
+}
